b6bbc412b724f5f9e156307b00374a4d8929e9ca
zike2007 <165039885+zike2007@users.noreply.github.com>
2024-06-13 11:59:34 -0300

    .

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Julia", "Dayana", "Maria Cecilia", "Mathew", "Ana Clara", "Guilhermo", "Gabriela"];
-
-export function aleatorio (lista){
-const posicao = Math.floor(Math.random()* lista.length);
-return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
